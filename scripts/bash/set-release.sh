b5bdbff16faa3d7b7a72e921821f43d636fdda36
Denilson Telaroli <ddts80@gmail.com>
2024-07-13 16:59:57 -0300

    fix: sed

--- scripts/bash/set-release.sh
+++ scripts/bash/set-release.sh
@@ -4,5 +4,5 @@ export TARGET_REVISION=$1
 export RELEASE_TAG=$2
 export FILE=$3
 
-sed -i '' "s/targetRevision:\s.*/targetRevision: ${TARGET_REVISION}/g" ${FILE}
-sed -i '' "s/tag:\s.*/tag: ${RELEASE_TAG}/g" ${FILE}
+sed -i "s/targetRevision:\s.*/targetRevision: ${TARGET_REVISION}/g" ${FILE}
+sed -i "s/tag:\s.*/tag: ${RELEASE_TAG}/g" ${FILE}
