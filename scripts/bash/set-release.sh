6888e9583178e7804580a89a1afdebfeff614f82
Denilson Telaroli <ddts80@gmail.com>
2024-07-13 18:33:52 -0300

    fix: test

--- scripts/bash/set-release.sh
+++ scripts/bash/set-release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export TARGET_REVISION=$1
-export TAG_NAME=$2
-export FILE=$3
+export TARGET_REVISION="$1"
+export TAG_NAME="$2"
+export FILE="$3"
 
-sed -i "s/targetRevision:\s.*/targetRevision: ${TARGET_REVISION}/g" ${FILE}
-sed -i "s/tag:\s.*/tag: ${TAG_NAME}/g" ${FILE}
+sed -i "s|targetRevision:\s.*|targetRevision: ${TARGET_REVISION}|g" ${FILE}
+sed -i "s|tag:\s.*|tag: ${TAG_NAME}|g" ${FILE}
