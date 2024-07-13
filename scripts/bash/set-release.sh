af884f97cb7cf88093468000ba41d859c94b4b76
Denilson Telaroli <ddts80@gmail.com>
2024-07-13 19:34:56 -0300

    feat: invert generator

--- scripts/bash/set-release.sh
+++ scripts/bash/set-release.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 export TARGET_REVISION="$1"
-export TAG_NAME="$2"
-export FILE="$3"
+export SHORT_COMMIT="$2"
+export TAG_NAME="$3"
+export FILE="$4"
 
 sed -i "s|targetRevision:\s.*|targetRevision: ${TARGET_REVISION}|g" ${FILE}
+sed -i "s|shortCommit:\s.*|shortCommit: ${SHORT_COMMIT}|g" ${FILE}
 sed -i "s|tag:\s.*|tag: ${TAG_NAME}|g" ${FILE}
