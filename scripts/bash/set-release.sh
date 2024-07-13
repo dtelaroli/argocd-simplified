45a633bd90d3f0ab84b61a4dd8e1ec06e6c9c1fd
Denilson Telaroli <ddts80@gmail.com>
2024-07-13 19:57:43 -0300

    feat: refs

--- scripts/bash/set-release.sh
+++ scripts/bash/set-release.sh
@@ -5,6 +5,6 @@ export SHORT_COMMIT="$2"
 export TAG_NAME="$3"
 export FILE="$4"
 
-sed -i "s|targetRevision:\s.*|targetRevision: ${TARGET_REVISION}|g" ${FILE}
-sed -i "s|shortCommit:\s.*|shortCommit: ${SHORT_COMMIT}|g" ${FILE}
-sed -i "s|tag:\s.*|tag: ${TAG_NAME}|g" ${FILE}
+sed -i '' "s|targetRevision:.*|targetRevision: ${TARGET_REVISION}|g" ${FILE}
+sed -i '' "s|shortCommit:.*|shortCommit: ${SHORT_COMMIT}|g" ${FILE}
+sed -i '' "s|tag:.*|tag: ${TAG_NAME}|g" ${FILE}
